8b44a9ed67eaf890bd0136f3f367859d7c4475d0
April Chang <aprilc@usc.edu>
2021-03-14 08:07:21 +0800

    add hw2

---
+++ hw2-starterCode/hw1-starterCode/README.md
@@ -0,0 +1,20 @@
+# CSCI 420 Assignment 1: Height Fields using Shaders
+
+## Description:
+
+This program generates a heightfield from a grayscale jpeg input. User can translate, rotate, and scale the rendered heightfields with mouse and keyboard, and can switch between 4 different modes including points, wireframe, solid, and smoothed solid.
+
+## Controls:
+
+control x,y translation via the left mouse button  
+control z translation via the middle mouse button  
+control x,y rotation via the left mouse button  
+control z rotation via the middle mouse button  
+control x,y scaling via the left mouse button  
+control z scaling via the middle mouse button  
+1: Switch to Point mode  
+2: Switch to Wireframe mode  
+3: Switch to Solid mode  
+4: Switch to Smoothed Solid mode  
+a: Start recording. Saves 300 screenshots to /animation  
+x: Take a screenshot and save it
---
+++ hw2-starterCode/hw1-starterCode/hw1.cpp
@@ -0,0 +1,467 @@
+﻿/*
+  CSCI 420 Computer Graphics, USC
+  Assignment 2: Roller Coaster
+  C++ starter code
+
+  Student username: aprilc
+*/
+
+#include "basicPipelineProgram.h"
+#include "openGLMatrix.h"
+#include "imageIO.h"
+#include "openGLHeader.h"
+#include "glutHeader.h"
+
+#include <iostream>
+#include <cstring>
+#include <vector>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#if defined(WIN32) || defined(_WIN32)
+  #ifdef _DEBUG
+    #pragma comment(lib, "glew32d.lib")
+  #else
+    #pragma comment(lib, "glew32.lib")
+  #endif
+#endif
+
+#if defined(WIN32) || defined(_WIN32)
+  char shaderBasePath[1024] = SHADER_BASE_PATH;
+#else
+  char shaderBasePath[1024] = "../openGLHelper-starterCode";
+#endif
+
+using namespace std;
+
+int mousePos[2]; // x,y coordinate of the mouse position
+
+int leftMouseButton = 0; // 1 if pressed, 0 if not 
+int middleMouseButton = 0; // 1 if pressed, 0 if not
+int rightMouseButton = 0; // 1 if pressed, 0 if not
+
+typedef enum { ROTATE, TRANSLATE, SCALE } CONTROL_STATE;
+CONTROL_STATE controlState = ROTATE;
+
+// state of the world
+float landRotate[3] = { 0.0f, 0.0f, 0.0f };
+float landTranslate[3] = { 0.0f, 0.0f, 0.0f };
+float landScale[3] = { 1.0f, 1.0f, 1.0f };
+
+int windowWidth = 1280;
+int windowHeight = 720;
+char windowTitle[512] = "CSCI 420 homework II";
+
+OpenGLMatrix matrix; 
+BasicPipelineProgram * pipelineProgram;
+
+int imageWidth, imageHeight;
+
+int numScreenshots = 0;
+bool startRecord = false;
+
+int numVertices = 0;
+GLuint vbo, vao;
+std::vector<glm::vec3> positions;
+
+
+// write a screenshot to the specified filename
+void saveScreenshot(const char * filename)
+{
+  unsigned char * screenshotData = new unsigned char[windowWidth * windowHeight * 3];
+  glReadPixels(0, 0, windowWidth, windowHeight, GL_RGB, GL_UNSIGNED_BYTE, screenshotData);
+
+  ImageIO screenshotImg(windowWidth, windowHeight, 3, screenshotData);
+
+  if (screenshotImg.save(filename, ImageIO::FORMAT_JPEG) == ImageIO::OK)
+    cout << "File " << filename << " saved successfully." << endl;
+  else cout << "Failed to save file " << filename << '.' << endl;
+
+  delete [] screenshotData;
+}
+
+void displayFunc()
+{
+  // render some stuff...
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+  matrix.SetMatrixMode(OpenGLMatrix::ModelView);
+  matrix.LoadIdentity();
+  int imageCenter = imageWidth / 2;
+  matrix.LookAt(imageCenter, imageCenter*3, -imageCenter, imageCenter, 0, -imageCenter, 0, 0, 1);
+ 
+  // Translate, Rotate, Scale
+  matrix.Translate(landTranslate[0], landTranslate[1], landTranslate[2]);
+  matrix.Rotate(landRotate[0], 1, 0, 0);
+  matrix.Rotate(landRotate[1], 0, 1, 0);
+  matrix.Rotate(landRotate[2], 0, 0, -1);
+  matrix.Scale(landScale[0], landScale[1], landScale[2]);
+
+  float m[16];
+  matrix.SetMatrixMode(OpenGLMatrix::ModelView);
+  matrix.GetMatrix(m);
+
+  float p[16];
+  matrix.SetMatrixMode(OpenGLMatrix::Projection);
+  matrix.GetMatrix(p);
+
+  // bind shader
+  pipelineProgram->Bind();
+
+  // set variable
+  pipelineProgram->SetModelViewMatrix(m);
+  pipelineProgram->SetProjectionMatrix(p);
+
+  // bind the VAO
+  glBindVertexArray(vao);
+  glDrawArrays(GL_POINTS, 0, numVertices);
+
+  // unbind the VAO
+  glBindVertexArray(0);
+
+  glutSwapBuffers();
+}
+
+void idleFunc()
+{
+  // when user hits 'a', starts taking screenshots until there are 300 
+	if (startRecord && numScreenshots <= 300)
+	{
+		char fileName[5];
+		sprintf(fileName, "%03d", numScreenshots);
+		saveScreenshot(("animation/" + std::string(fileName) + ".jpg").c_str());
+		numScreenshots++;
+	}
+  glutPostRedisplay();
+}
+
+void reshapeFunc(int w, int h)
+{
+  glViewport(0, 0, w, h);
+
+  matrix.SetMatrixMode(OpenGLMatrix::Projection);
+  matrix.LoadIdentity();
+  matrix.Perspective(54.0f, (float)w / (float)h, 0.01f, 1000.0f);
+
+  // set the mode back to ModelView
+  matrix.SetMatrixMode(OpenGLMatrix::ModelView);
+}
+
+void mouseMotionDragFunc(int x, int y)
+{
+  // mouse has moved and one of the mouse buttons is pressed (dragging)
+
+  // the change in mouse position since the last invocation of this function
+  int mousePosDelta[2] = { x - mousePos[0], y - mousePos[1] };
+
+  switch (controlState)
+  {
+    // translate the landscape
+    case TRANSLATE:
+      if (leftMouseButton)
+      {
+        // control x,y translation via the left mouse button
+        landTranslate[0] += mousePosDelta[0] * 0.01f;
+        landTranslate[1] -= mousePosDelta[1] * 0.01f;
+      }
+      if (middleMouseButton)
+      {
+        // control z translation via the middle mouse button
+        landTranslate[2] += mousePosDelta[1] * 0.01f;
+      }
+      break;
+
+    // rotate the landscape
+    case ROTATE:
+      if (leftMouseButton)
+      {
+        // control x,y rotation via the left mouse button
+        landRotate[0] += mousePosDelta[1];
+        landRotate[1] += mousePosDelta[0];
+      }
+      if (middleMouseButton)
+      {
+        // control z rotation via the middle mouse button
+        landRotate[2] += mousePosDelta[1];
+      }
+      break;
+
+    // scale the landscape
+    case SCALE:
+      if (leftMouseButton)
+      {
+        // control x,y scaling via the left mouse button
+        landScale[0] *= 1.0f + mousePosDelta[0] * 0.01f;
+        landScale[1] *= 1.0f - mousePosDelta[1] * 0.01f;
+      }
+      if (middleMouseButton)
+      {
+        // control z scaling via the middle mouse button
+        landScale[2] *= 1.0f - mousePosDelta[1] * 0.01f;
+      }
+      break;
+  }
+
+  // store the new mouse position
+  mousePos[0] = x;
+  mousePos[1] = y;
+}
+
+void mouseMotionFunc(int x, int y)
+{
+  // mouse has moved
+  // store the new mouse position
+  mousePos[0] = x;
+  mousePos[1] = y;
+}
+
+void mouseButtonFunc(int button, int state, int x, int y)
+{
+  // a mouse button has has been pressed or depressed
+
+  // keep track of the mouse button state, in leftMouseButton, middleMouseButton, rightMouseButton variables
+  switch (button)
+  {
+    case GLUT_LEFT_BUTTON:
+      leftMouseButton = (state == GLUT_DOWN);
+    break;
+
+    case GLUT_MIDDLE_BUTTON:
+      middleMouseButton = (state == GLUT_DOWN);
+    break;
+
+    case GLUT_RIGHT_BUTTON:
+      rightMouseButton = (state == GLUT_DOWN);
+    break;
+  }
+
+  // keep track of whether CTRL and SHIFT keys are pressed
+  switch (glutGetModifiers())
+  {
+    case GLUT_ACTIVE_CTRL:
+      controlState = TRANSLATE;
+    break;
+
+    case GLUT_ACTIVE_SHIFT:
+      controlState = SCALE;
+    break;
+
+    // if CTRL and SHIFT are not pressed, we are in rotate mode
+    default:
+      controlState = ROTATE;
+    break;
+  }
+
+  // store the new mouse position
+  mousePos[0] = x;
+  mousePos[1] = y;
+}
+
+void keyboardFunc(unsigned char key, int x, int y)
+{
+  switch (key)
+  {
+    case 27: // ESC key
+      exit(0); // exit the program
+    break;
+
+    case ' ':
+      cout << "You pressed the spacebar." << endl;
+    break;
+
+    case 'x':
+      // take a screenshot
+      saveScreenshot("screenshot.jpg");
+    break;
+
+	case 'a':
+		cout << "You pressed A. Start recording..." << endl;
+		startRecord = true;
+		break;
+
+  }
+}
+
+// represents one control point along the spline 
+struct Point
+{
+	double x;
+	double y;
+	double z;
+};
+
+// spline struct 
+// contains how many control points the spline has, and an array of control points 
+struct Spline
+{
+	int numControlPoints;
+	Point* points;
+};
+
+// the spline array 
+Spline* splines;
+// total number of splines 
+int numSplines;
+
+int loadSplines(char* argv)
+{
+	char* cName = (char*)malloc(128 * sizeof(char));
+	FILE* fileList;
+	FILE* fileSpline;
+	int iType, i = 0, j, iLength;
+
+	// load the track file 
+	fileList = fopen(argv, "r");
+	if (fileList == NULL)
+	{
+		printf("can't open file\n");
+		exit(1);
+	}
+
+	// stores the number of splines in a global variable 
+	fscanf(fileList, "%d", &numSplines);
+
+	splines = (Spline*)malloc(numSplines * sizeof(Spline));
+
+	// reads through the spline files 
+	for (j = 0; j < numSplines; j++)
+	{
+		i = 0;
+		fscanf(fileList, "%s", cName);
+		fileSpline = fopen(cName, "r");
+
+		if (fileSpline == NULL)
+		{
+			printf("can't open file\n");
+			exit(1);
+		}
+
+		// gets length for spline file
+		fscanf(fileSpline, "%d %d", &iLength, &iType);
+
+		// allocate memory for all the points
+		splines[j].points = (Point*)malloc(iLength * sizeof(Point));
+		splines[j].numControlPoints = iLength;
+
+		// saves the data to the struct
+		while (fscanf(fileSpline, "%lf %lf %lf",
+			&splines[j].points[i].x,
+			&splines[j].points[i].y,
+			&splines[j].points[i].z) != EOF)
+		{
+			i++;
+		}
+	}
+
+	free(cName);
+
+	return 0;
+}
+
+int initTexture(const char* imageFilename, GLuint textureHandle)
+{
+	// read the texture image
+	ImageIO img;
+	ImageIO::fileFormatType imgFormat;
+	ImageIO::errorType err = img.load(imageFilename, &imgFormat);
+
+	if (err != ImageIO::OK)
+	{
+		printf("Loading texture from %s failed.\n", imageFilename);
+		return -1;
+	}
+
+	// check that the number of bytes is a multiple of 4
+	if (img.getWidth() * img.getBytesPerPixel() % 4)
+	{
+		printf("Error (%s): The width*numChannels in the loaded image must be a multiple of 4.\n", imageFilename);
+		return -1;
+	}
+
+	// allocate space for an array of pixels
+	int width = img.getWidth();
+	int height = img.getHeight();
+	unsigned char* pixelsRGBA = new unsigned char[4 * width * height]; // we will use 4 bytes per pixel, i.e., RGBA
+
+	// fill the pixelsRGBA array with the image pixels
+	memset(pixelsRGBA, 0, 4 * width * height); // set all bytes to 0
+	for (int h = 0; h < height; h++)
+		for (int w = 0; w < width; w++)
+		{
+			// assign some default byte values (for the case where img.getBytesPerPixel() < 4)
+			pixelsRGBA[4 * (h * width + w) + 0] = 0; // red
+			pixelsRGBA[4 * (h * width + w) + 1] = 0; // green
+			pixelsRGBA[4 * (h * width + w) + 2] = 0; // blue
+			pixelsRGBA[4 * (h * width + w) + 3] = 255; // alpha channel; fully opaque
+
+			// set the RGBA channels, based on the loaded image
+			int numChannels = img.getBytesPerPixel();
+			for (int c = 0; c < numChannels; c++) // only set as many channels as are available in the loaded image; the rest get the default value
+				pixelsRGBA[4 * (h * width + w) + c] = img.getPixel(w, h, c);
+		}
+
+	// bind the texture
+	glBindTexture(GL_TEXTURE_2D, textureHandle);
+
+	// initialize the texture
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixelsRGBA);
+
+	// generate the mipmaps for this texture
+	glGenerateMipmap(GL_TEXTURE_2D);
+
+	// set the texture parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	// query support for anisotropic texture filtering
+	GLfloat fLargest;
+	glGetFloatv(GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT, &fLargest);
+	printf("Max available anisotropic samples: %f\n", fLargest);
+	// set anisotropic texture filtering
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY_EXT, 0.5f * fLargest);
+
+	// query for any errors
+	GLenum errCode = glGetError();
+	if (errCode != 0)
+	{
+		printf("Texture initialization error. Error code: %d.\n", errCode);
+		return -1;
+	}
+
+	// de-allocate the pixel array -- it is no longer needed
+	delete[] pixelsRGBA;
+
+	return 0;
+}
+
+// Note: You should combine this file
+// with the solution of homework 1.
+
+// Note for Windows/MS Visual Studio:
+// You should set argv[1] to track.txt.
+// To do this, on the "Solution Explorer",
+// right click your project, choose "Properties",
+// go to "Configuration Properties", click "Debug",
+// then type your track file name for the "Command Arguments".
+// You can also repeat this process for the "Release" configuration.
+
+int main(int argc, char** argv)
+{
+	if (argc < 2)
+	{
+		printf("usage: %s <trackfile>\n", argv[0]);
+		exit(0);
+	}
+
+	// load the splines from the provided filename
+	loadSplines(argv[1]);
+
+	printf("Loaded %d spline(s).\n", numSplines);
+	for (int i = 0; i < numSplines; i++)
+		printf("Num control points in spline %d: %d.\n", i, splines[i].numControlPoints);
+
+	return 0;
+}
